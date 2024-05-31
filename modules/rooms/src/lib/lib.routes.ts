892299fc765f006991ce9fcc97a81aa234708e7e
Vitor Euzebio <vitor.v.euzebio@gmail.com>
2024-05-31 14:50:52 -0300

    First commit

---
+++ modules/rooms/src/lib/lib.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { RoomsComponent } from './rooms.component';
+
+export const roomsRoutes: Route[] = [{ path: '', component: RoomsComponent }];
---
+++ modules/rooms/src/lib/rooms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'rm-rooms',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './rooms.component.html',
+  styleUrl: './rooms.component.scss',
+})
+export class RoomsComponent {}
